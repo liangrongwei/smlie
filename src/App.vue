2c4debd52f0f83850765e74216e9b5da9e8821b1
liangrongwei <39259994+liangrongwei@users.noreply.github.com>
2018-05-14 17:14:26 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+    <router-link to="/gundong">222</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,209 @@
+<template>
+  <div>
+    <!--头部搜索-->
+     <div class="search-bar">
+            <van-row>
+                <van-col span="3">
+                    <img :src="locationIcon" width="80%" class="location-icon"/>
+                </van-col>
+                <van-col span="16">
+                    <input type="text" class="search-input" />
+                </van-col>
+                <van-col span="5">
+                    <van-button size="mini">查找</van-button>
+                </van-col>
+            </van-row>
+        </div>
+        <!--vant组件轮播van-swipe-->
+        <div class="swiper-area">
+          <van-swipe :autoplay="1000">
+            <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+              <img v-lazy="banner.image" width="100%">
+            </van-swipe-item>
+          </van-swipe>  
+        </div>
+        <!--分类-->
+        <div class="type-bar">
+            <div v-for="(cate,index) in category" :key="index">
+                <img v-lazy="cate.image" width="90%">
+                <span>{{cate.mallCategoryName}}</span>
+
+            </div>
+        </div>  
+       <!--广告图片-->
+      <div class="ad-banner">
+    <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
+      </div>
+      <!--vue组件vue-awesome-swiper轮播-->
+      <div class="recommend-area">
+            <div class="recommend-title">
+                商品推荐
+            </div>
+            <div class="recommend-body">
+                <swiper :options="swiperOption" >
+                   <swiper-slide v-for="(item,index) in recommendGoods " :key="index" >
+                       <div class="recommend-item">
+                           <img :src="item.image" width="80%">
+                           <div>{{item.goodsName}}</div>
+                           <div>￥{{item.price}}(￥{{item.mallPrice}})</div>
+                       </div>
+                   </swiper-slide> 
+                   
+                </swiper>
+            </div>
+        </div>
+       
+<floorComponent :floorData="floor1" :floorTitle="floorName.floor1"></floorComponent>
+<floorComponent :floorData="floor2" :floorTitle="floorName.floor3"></floorComponent>
+<floorComponent :floorData="floor3" :floorTitle="floorName.floor2"></floorComponent>
+  </div>
+</template>
+
+<script>
+import floorComponent from './component/floorComponent'
+import axios from 'axios'
+import 'swiper/dist/css/swiper.css'
+import {swiper , swiperSlide} from 'vue-awesome-swiper'
+export default {
+  
+  data () {
+    return {
+      swiperOption:{
+        slidesPerView:3,
+       },
+      locationIcon: require('../assets/location.png'),
+      adBanner:'',
+      recommendGoods:[],
+      category:[],
+      floor1:[],         
+      floor2:[],         
+      floor3:[], 
+      bannerPicArray:[],
+      floorName:[]
+    }
+     
+  },
+  components:{swiper,swiperSlide,floorComponent},
+  created(){
+      axios({
+        url:'https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/index',
+        method:'get',
+        
+      })
+      .then(response=>{
+        console.log(response)
+        if(response.status==200){
+          this.category=response.data.data.category;
+          this.bannerPicArray = response.data.data.slides;
+          this.adBanner = response.data.data.advertesPicture;
+          this.recommendGoods = response.data.data.recommend;
+          this.floorName = response.data.data.floorName;
+          this.floor1 = response.data.data.floor1              
+          this.floor2 = response.data.data.floor2              
+          this.floor3 = response.data.data.floor3
+        }
+      })
+      .catch(error=>{
+        console.log(error)
+      })
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .search-bar{
+        height:2.2rem;
+        background-color: #e5017d;
+        line-height: 2.2rem;
+        overflow: hidden;
+    }
+    .search-input{
+        width:100%;
+        height: 1.3rem;
+        border-top:0px;
+        border-left:0px;
+        border-right:0px;
+        border-bottom:1px solid #fff !important;
+        background-color: #e5017d;
+        color:#fff;
+    }
+    .location-icon{
+        padding-top:.2rem;
+        padding-left:.3rem;
+    }
+    .swiper-area{
+        clear:both;
+        max-height:15rem;
+        overflow: hidden;
+    }
+    .type-bar{
+        background-color: #fff;
+        margin:0 .3rem .3rem .3rem;
+        border-radius: .3rem;
+        font-size:14px;
+        display:flex;
+        flex-direction:row;
+        flex-wrap:nowrap;
+    }
+    .type-bar div{
+        padding:.3rem;
+        font-size:12px;
+        text-align: center;
+    }
+    .recommend-area{
+        background-color: #fff;
+        margin-top:.3rem;
+    }
+    .recommend-title{
+        border-bottom:1px solid #eee;
+        font-size:14px;
+        padding:.2rem;
+        color:#e5017d;
+        text-align: left;
+    }
+    .recommend-body{
+        border-bottom:1px solid #eee;
+    }
+    .recommend-item{
+        width:99%;
+        border-right:1px solid #eee;
+        font-size:12px;
+        text-align: center;
+    }
+    .floor-anomaly{
+      display: flex;
+      flex-direction:row;
+      background-color: #fff;
+      border-bottom:1px solid #ddd;
+  }
+  .floor-anomaly div{
+     width:10rem;
+ 
+     box-sizing: border-box;
+     -webkit-box-sizing: border-box;
+  }
+  .floor-one{
+      border-right:1px solid #ddd;
+ 
+  }
+  .floor-two{
+      border-bottom:1px solid #ddd;
+  }
+  .floor-rule{
+      display: flex;
+      flex-direction: row;
+      flex-wrap:wrap;
+      background-color: #fff;
+ 
+  }
+  .floor-rule div{
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+      width:10rem;
+      border-bottom:1px solid #ddd;
+  }
+  .floor-rule div:nth-child(odd){
+      border-right: 1px solid #ddd;
+  }
+</style>
